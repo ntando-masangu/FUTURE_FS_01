368d4e27a4dbb016105f063bd0724b3a4d859d0d
Ntando Masangu <officialnmasango@gmail.com>
2026-02-17 16:07:51 +0200

    Fixed source code link

--- src/data/project.js
+++ src/data/project.js
@@ -8,7 +8,7 @@ const projects = [
       "A responsive website portfolio built with React.js, showcasing my projects, skills, and contact information. It features smooth scrolling, a dark mode toggle, and a clean, creative, modern design.",
     technologies: ["React.js", "CSS3", "HTML5"],
     image: project1,
-    repositoryLink: "https://github.com/ntando-masangu/FUTURE_FS_01: ",
+    repositoryLink: "https://github.com/ntando-masangu/FUTURE_FS_01",
     liveDemoLink: "https://ntando-masangu-portfolio.vercel.app/",
   },
 ];
